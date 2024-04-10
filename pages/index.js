070af08e5daa4ce8ef84d78d7233d91ddfce87e8
Matheus Guimarães <53066165+manguima@users.noreply.github.com>
2024-04-10 20:46:21 +0000

    Criado pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
 function Home() {
-  return <h1>O que é um ano para algo que será a vida inteira.</h1>;
+  return (
+    <html style={{ paddinig: "0px", margin: "0px", border: "0px" }}>
+      <body style={{ paddinig: "0px", margin: "0px", border: "0px" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "90dvh",
+          }}
+        >
+          <p
+            style={{
+              fontSize: "4rem",
+              fontFamily: "sans-serif",
+              fontWeight: 900,
+            }}
+          >
+            Em Construção.
+          </p>
+        </div>
+      </body>
+    </html>
+  );
 }
 
 export default Home;
