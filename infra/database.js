893471b343307b4dc223ac26f1cc95a0f14e48a3
Matheus Guimarães <53066165+manguima@users.noreply.github.com>
2024-04-19 20:46:53 +0000

    Adicionado ssl na  conexao com banco

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,22 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("Credenciais de acesso Postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: true,
+  });
+
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
     console.log(err);
+    throw error;
   } finally {
     await client.end();
   }
