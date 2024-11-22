6d3054c9777ff80607b629e678db8978c463e3a1
Matheus Guimarães <dredhors@hotmail.com>
2024-11-22 10:26:23 -0300

    adds `/api/v1/migrations endpoint`

--- infra/database.js
+++ infra/database.js
@@ -1,27 +1,40 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
+  let client;
 
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
     throw error;
   } finally {
     await client.end();
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+
+  return client;
+}
+
+function getSSLValues() {
+  return process.env.NODE_ENV === "production" ? true : false;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
